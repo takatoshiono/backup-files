ed0292bfdbb9e0bf83766198ba0fc423e613eaf0
Takatoshi Ono <takatoshi.ono@gmail.com>
2014-06-24 00:33:25 +0900

    Add a test for #transport using vcr

--- Gemfile
+++ Gemfile
@@ -2,5 +2,7 @@
 source "https://rubygems.org"
 
 gem 'aws-sdk'
-gem 'rspec'
 gem 'dotenv'
+gem 'rspec'
+gem 'vcr'
+gem 'webmock'
--- spec/lib/file_transporter/s3_spec.rb
+++ spec/lib/file_transporter/s3_spec.rb
@@ -1,7 +1,32 @@
 require 'file_transporter/s3'
+require 'vcr'
+require 'dotenv'
+
+VCR.configure do |c|
+  c.cassette_library_dir = File.expand_path('./spec/fixtures/vcr_cassettes')
+  c.hook_into :webmock
+end
+
+RSpec.configure do |c|
+  c.around(:each) do |example|
+    VCR.use_cassette(example.metadata[:full_description]) do
+      example.run
+    end
+  end
+end
+
+Dotenv.load
 
 describe FileTransporter::S3 do
   describe '#transport' do
+    let(:file_path) { File.expand_path('../../../files/2014-06-24/clip-2014-06-24 00:16;00;00.mov', __FILE__) }
+    let(:content) { File.read(file_path) }
+
+    it 'uploads a file to Amazon S3' do
+      FileTransporter::S3.transport(file_path)
+      object_key_name = FileTransporter::S3.object_key_name(file_path)
+      expect(FileTransporter::S3.bucket.objects[object_key_name].read).to eq content
+    end
   end
 
   describe '#bucket' do
